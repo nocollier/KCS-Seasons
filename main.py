33e50d1214e71ae7808569bab79e74b017b2d580
Nathan Collier <nathaniel.collier@gmail.com>
2026-05-15 11:01:07 -0400

    updates with things I found in my email

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def repertoire_list(yaml_source: Path) -> dict[str, list[str]]:
     for _, concerts in src.items():
         for concert in concerts:
             _, details = next(iter(concert.items()))
+            if "repertoire" not in details:
+                continue
             for music in details["repertoire"]:
                 composer = music["composer"].replace("arr.", "").strip()
                 if composer not in out:
@@ -21,11 +23,23 @@ def repertoire_list(yaml_source: Path) -> dict[str, list[str]]:
 
 
 def main():
-    rep = repertoire_list(Path("knoxville_choral_society.yaml"))
-    keys = sorted(rep.keys(), key=lambda k: k.split()[-1].lower())
-    for key in keys:
-        print(f"{key}: {', '.join(sorted([f'"{t}"' for t in rep[key]]))}")
+    for yml in Path(".").glob("*.yaml"):
+        if yml.name.startswith("."):
+            continue
+        print(f"--- {' '.join(t.capitalize() for t in yml.stem.split('_'))} ---")
+        rep = repertoire_list(yml)
+        keys = sorted(rep.keys(), key=lambda k: k.split()[-1].lower())
+        for key in keys:
+            print(f"{key}: {', '.join(sorted([f'"{t}"' for t in rep[key]]))}")
 
+
+"""
+2022-2023
+10/20/2022 - CONCERT: St. John's Episcopal Cathedral - 7:30p.m.
+
+
+
+"""
 
 if __name__ == "__main__":
     main()
