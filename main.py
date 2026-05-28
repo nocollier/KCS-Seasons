c6a4e97e265f45abe77be4b446c8274e0d324482
Nathan Collier <nathaniel.collier@gmail.com>
2026-05-28 11:01:42 -0400

    updates with the cathedral youtube find

--- main.py
+++ main.py
@@ -22,22 +22,45 @@ def repertoire_list(yaml_source: Path) -> dict[str, list[str]]:
     return out
 
 
+def per_season(yaml_source: Path) -> dict[str, dict[str, list[str]]]:
+    with open(yaml_source, "r") as fin:
+        src = yaml.safe_load(fin)
+    by_season = {}
+    for season, concerts in src.items():
+        out = {}
+        for concert in concerts:
+            _, details = next(iter(concert.items()))
+            if "repertoire" not in details:
+                continue
+            for music in details["repertoire"]:
+                composer = music["composer"].replace("arr.", "").strip()
+                if composer not in out:
+                    out[composer] = []
+                title = music["title"].strip()
+                if title not in out[composer]:
+                    out[composer] += [title]
+        by_season[season] = out
+    return by_season
+
+
 def main():
-    for yml in Path(".").glob("*.yaml"):
-        if yml.name.startswith("."):
-            continue
+    by_season = per_season(Path("knoxville_choral_society.yaml"))
+    for season, rep in by_season.items():
+        print(season)
+        keys = sorted(rep.keys(), key=lambda k: k.split()[-1].lower())
+        for key in keys:
+            print(f"{key.split(' ')[-1]}: {', '.join(sorted([t for t in rep[key]]))}")
+
+    if 0:
+        for yml in Path(".").glob("*.yaml"):
+            if yml.name.startswith("."):
+                continue
         print(f"--- {' '.join(t.capitalize() for t in yml.stem.split('_'))} ---")
         rep = repertoire_list(yml)
         keys = sorted(rep.keys(), key=lambda k: k.split()[-1].lower())
         for key in keys:
             print(f"{key}: {', '.join(sorted([f'"{t}"' for t in rep[key]]))}")
 
-
-"""
-2022-2023
-10/20/2022 - CONCERT: St. John's Episcopal Cathedral - 7:30p.m.
-
-"""
 
 if __name__ == "__main__":
     main()
